27450ea6b35d196fbc2b1a5cb6cff528ceeaf4fb
Vitryssen <andre1.nordlund@gmail.com>
2021-02-11 13:38:36 +0100

    Update

--- src/labb3/Readers/FriendsReader.java
+++ src/labb3/Readers/FriendsReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Readers;
+package labb3.Readers;
 
 import labb3.DataStructures.Friend;
 import java.io.BufferedReader;
